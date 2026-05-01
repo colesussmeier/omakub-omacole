7a911c5b6390869543c06158d774766dfeda42c3
jerry <colesussmeier@gmail.com>
2026-05-01 15:12:42 -0400

    steam install fixes

--- install/desktop/optional/app-steam.sh
+++ install/desktop/optional/app-steam.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Play games from https://store.steampowered.com/
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y libgl1:i386 libdrm2:i386
+
 cd /tmp
 wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 sudo apt install -y ./steam.deb
